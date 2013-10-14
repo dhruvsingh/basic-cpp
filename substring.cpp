1eb1fd8353547c186d7cad39608acde66d076936
Dhruv Singh <dhruvsingh.er@gmail.com>
2013-10-14 21:55:28 +0530

    Update substring.cpp

--- substring.cpp
+++ substring.cpp
@@ -6,7 +6,6 @@
         
         int main()
         {
-        //clrscr();
         char string[10],substr[10],str1[10];
         int len,lensub,i,j,k,flag=1;
         cout<<"\nEnter the string : ";
@@ -15,18 +14,14 @@
         cin>>substr;
         len= strlen(string);
     	lensub= strlen(substr);
-        cout<<"\nlength of sub-string : "<<lensub;
-        cout<<"\nlength of string : "<<len;
         for(i=0;i<len;)
         {
-    		for(k=0,j=i;k<lensub;j++,k++)
-    		 str1[k]=string[j];
-    	 str1[k]='\0';
-        //cout<<"\nThe str1 is: "<<str1;
+	        for(k=0,j=i;k<lensub;j++,k++)
+		   str1[k]=string[j];
+    	    str1[k]='\0';
     		if((strcmp(substr,str1))==0)
     		{
     		 cout<<"\n\nSubstring found at location :"<<i+1;
-    		 //getchar();
              flag=0;
     		 break;
     		}
@@ -35,5 +30,6 @@
     	}
         if(flag!=0)
         cout<<"\n\nSub-string not found!";
+		getchar();
         return 0;
         }
